6e589cc913e0e9839b814b53d652eb9e1e63da14
ysk <kim206gh@naver.com>
2022-03-17 16:55:24 +0900

    if문 학습

---
+++ inflearn-go-introduction/src/section3/if1.go
@@ -0,0 +1,31 @@
+package main
+
+func main() {
+	//
+	//b := 0
+	//
+	//// 에러
+	//if b >= 25
+	//{
+	//	fmt.Println("b > 25 ")
+	//}
+	//
+	//// 에러
+	//if b >= 25
+	//	fmt.Println("b > 25")
+	//
+	//// 정상
+	//if b >= 25 {
+	//	fmt.Println("b > 25")
+	//}
+	//
+	////에러
+	//if c:=1; c {
+	//	fmt.Println("true")
+	//}
+	//
+	//// 정상
+	//if c :=40; c >= 35 { // c의 스코프는 if문 안에서만임.
+	//	fmt.Println("35이상")
+	//}
+}
---
+++ inflearn-go-introduction/src/section3/if2.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int = 50
+
+	b := 70
+
+	// 예제1
+	if a >= 65 {
+		fmt.Println("65 이상")
+	} else {
+		fmt.Println("65 이하")
+	}
+
+	// 예제2
+	if b >= 65 {
+		fmt.Println("65 이상")
+	} else {
+		fmt.Println("65 이하")
+	}
+
+	i := 100
+
+	if i >= 120 {
+
+	} else if i >= 100 && i< 120 {
+
+	} else if i < 100 && i >= 50 {
+
+	} else {
+
+	}
+
+}
