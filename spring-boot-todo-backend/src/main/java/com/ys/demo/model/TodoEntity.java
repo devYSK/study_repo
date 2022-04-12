19d5bd6ad647268b4c44a1731940ed029a42a884
devysk <kim206gh@naver.com>
2022-04-12 10:29:18 +0900

    init

---
+++ spring-boot-todo-backend/src/main/java/com/ys/demo/model/TodoEntity.java
@@ -0,0 +1,12 @@
+package com.ys.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder @NoArgsConstructor @AllArgsConstructor
+@Data
+public class TodoEntity {
+
+}
