19e07444231f7cb704b35fe2c0cab5131b889a43
ysk <kim206gh@naver.com>
2022-07-24 14:40:04 +0900

    mvc1편 학습 완료

---
+++ inf-spring-mvc1/mvc-item-service/src/test/java/com/ys/mvcitemservice/MvcItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.mvcitemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
