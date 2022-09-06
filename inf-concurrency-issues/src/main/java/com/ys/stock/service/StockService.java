7b19f4b54416920cb916c8c3f398bed9afae2eda
ysk <kim206gh@naver.com>
2022-09-06 23:53:51 +0900

    feat : git 관련 디렉토리 추가

--- inf-concurrency-issues/src/main/java/com/ys/stock/service/StockService.java
+++ inf-concurrency-issues/src/main/java/com/ys/stock/service/StockService.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @author : ysk
@@ -19,10 +18,8 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
-
     @Transactional(propagation = Propagation.REQUIRES_NEW) // syncronized 는 서버가 2대 이상일때 정합성을 보장해주지 못합니다.
     public void decrease(Long id, Long quantity) {
-
         Stock stock = stockRepository.findById(id).orElseThrow();
 
         stock.decrease(quantity);
