d5099c446ba61c76b73c2ab0e30e0f20c685b708
ysk <kim206gh@naver.com>
2023-03-30 04:00:03 +0900

    add . 학습 완료

--- nextstep-java/jwt-basic/src/main/java/com/ys/jwtbasic/core/jdbc/PreparedStatementSetter.java
+++
@@ -1,8 +0,0 @@
-package com.ys.jwtbasic.core.jdbc;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface PreparedStatementSetter {
-	void setParameters(PreparedStatement pstmt) throws SQLException;
-}
--- nextstep-java/jwt-basic/src/main/java/com/ys/jwtbasic/core/jdbc/RowMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ys.jwtbasic.core.jdbc;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface RowMapper<T> {
-	T mapRow(ResultSet rs) throws SQLException;
-}
