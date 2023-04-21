a9c34b1826cfa8d611df1a81102073fa2b04a516
ysk <kim206gh@naver.com>
2023-04-21 18:35:27 +0900

    add : 실전 자바 소프트웨어 개발 학습

---
+++ 실전_자바_소프트웨어_개발/src/main/java/com/iteratrlearning/shu_book/example/chapter_05/BusinessRuleEngine.java
@@ -0,0 +1,23 @@
+package com.iteratrlearning.shu_book.example.chapter_05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusinessRuleEngine {
+
+    private final List<Rule> rules;
+    private final Facts facts;
+
+    public BusinessRuleEngine(Facts facts) {
+        this.facts = facts;
+        this.rules = new ArrayList<>();
+    }
+
+    public void addRule(Rule rule) {
+        this.rules.add(rule);
+    }
+
+    public void run() {
+        this.rules.forEach(rule -> rule.perform(facts));
+    }
+}
