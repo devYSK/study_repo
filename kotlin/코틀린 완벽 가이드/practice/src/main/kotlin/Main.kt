da27dac622e0949b071cbab86c58d6678195f71b
ysk <kim206gh@naver.com>
2023-05-12 23:23:43 +0900

    add : 코루틴 학습

--- kotlin/코틀린 완벽 가이드/practice/src/main/kotlin/Main.kt
+++ kotlin/코틀린 완벽 가이드/practice/src/main/kotlin/Main.kt
@@ -11,93 +11,25 @@ class Coroutine {
 
 
 }
+suspend fun main() {
 
-sealed class AccountMessage
-
-class GetBalance(
-    val amount: CompletableDeferred<Long>
-) : AccountMessage()
-
-class Deposit(val amount: Long) : AccountMessage()
-
-class Withdraw(
-    val amount: Long,
-    val isPermitted: CompletableDeferred<Boolean>
-) : AccountMessage()
-
-fun CoroutineScope.accountManager(
-    initialBalance: Long
-) = actor<AccountMessage> {
-
-    var balance = initialBalance
-
-    for (message in channel) {
-        when (message) {
-            is GetBalance ->message.amount.complete(balance)
-
-            is Deposit -> {
-                balance += message.amount
-                println("Deposited ${message.amount}")
-            }
-
-            is Withdraw -> {
-                val canWithdraw = balance >= message.amount
-                if (canWithdraw) {
-                    balance -= message.amount
-                    println("Withdrawn ${message.amount}")
-                }
-                message.isPermitted.complete(canWithdraw)
-            }
-        }
+    val async = CoroutineScope(Dispatchers.Default).launch {
+        println("start launch")
+        delay(100)
+        println("end launch")
     }
-}
 
-private suspend fun SendChannel<AccountMessage>.deposit(
-    name: String,
-    amount: Long
-) {
-    send(Deposit(amount))
-    println("$name: deposit $amount")
-}
 
-private suspend fun SendChannel<AccountMessage>.tryWithdraw(
-    name: String,
-    amount: Long
-) {
-    val status = CompletableDeferred<Boolean>().let {
-        send(Withdraw(amount, it))
-        if (it.await()) "OK" else "DENIED"
+    val async1 = GlobalScope.launch {
+        println("launch1 start")
     }
-    println("$name: withdraw $amount ($status)")
-}
 
-private suspend fun SendChannel<AccountMessage>.printBalance(
-    name: String
-) {
-    val balance = CompletableDeferred<Long>().let {
-        send(GetBalance(it))
-        it.await()
+    val async2 = GlobalScope.launch {
+        println("launch2 start")
     }
-    println("$name: balance is $balance")
-}
 
-fun main() {
-    runBlocking {
-        val manager = accountManager(100)
-        withContext(Dispatchers.Default) {
-            launch {
-                manager.deposit("Client #1", 50)
-                manager.printBalance("Client #1")
-            }
-
-            launch {
-                manager.tryWithdraw("Client #2", 100)
-                manager.printBalance("Client #2")
-            }
-        }
-
-        manager.tryWithdraw("Client #0", 1000)
-        manager.printBalance("Client #0")
-        manager.close()
-    }
+    async.join()
+    async1.join()
+    async2.join()
+    println("main end")
 }
