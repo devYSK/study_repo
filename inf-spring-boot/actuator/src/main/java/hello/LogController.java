4cf7df3da8ed9ea057720a36c083ee0cafc020df
ysk <kim206gh@naver.com>
2023-06-09 15:10:20 +0900

    add : 액츄에이터 정리

---
+++ inf-spring-boot/actuator/src/main/java/hello/LogController.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class LogController {
+
+	@GetMapping("/log")
+	public String log() {
+		log.trace("trace log");
+		log.debug("debug log");
+		log.info("info log");
+		log.warn("warn log");
+		log.error("error log");
+		return "ok";
+	}
+
+}
