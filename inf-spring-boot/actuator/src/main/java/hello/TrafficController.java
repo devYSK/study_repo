e6ec5c09ac894975c87e64b30dd94ee24f0d4abd
ysk <kim206gh@naver.com>
2023-06-11 20:10:56 +0900

    add : 그라파나 모니터링 학습

---
+++ inf-spring-boot/actuator/src/main/java/hello/TrafficController.java
@@ -0,0 +1,57 @@
+package hello;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class TrafficController {
+	@GetMapping("/cpu")
+	public String cpu() {
+		log.info("cpu");
+		long value = 0;
+		for (long i = 0; i < 100000000000L; i++) {
+			value++;
+		}
+		return "ok value=" + value;
+	}
+
+	private List<String> list = new ArrayList<>();
+
+	@GetMapping("/jvm")
+	public String jvm() {
+		log.info("jvm");
+		for (int i = 0; i < 1000000; i++) {
+			list.add("hello jvm!" + i);
+		}
+		return "ok";
+	}
+
+	@Autowired
+	DataSource dataSource;
+
+	@GetMapping("/jdbc")
+	public String jdbc() throws SQLException {
+		log.info("jdbc");
+		Connection conn = dataSource.getConnection();
+		log.info("connection info={}", conn);
+		//conn.close(); //커넥션을 닫지 않는다.
+		return "ok";
+	}
+
+	@GetMapping("/error-log")
+	public String errorLog() {
+		log.error("error log");
+		return "error";
+	}
+}
