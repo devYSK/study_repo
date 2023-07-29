2460f268c12d8cdbbc07924e7eb0327a005176fe
ysk <kim206gh@naver.com>
2023-07-29 20:37:21 +0900

    add : 9장 스프링 컨피규레이션

--- 만들면서 배우는 클린 아키텍처/src/main/java/com/ys/cleanarchiecture/common/PersistenceAdapter.java
+++ 만들면서 배우는 클린 아키텍처/src/main/java/com/ys/cleanarchiecture/common/PersistenceAdapter.java
@@ -6,8 +6,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AliasFor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
+
+import com.ys.cleanarchiecture.adapter.persistence.AccountMapper;
+import com.ys.cleanarchiecture.adapter.persistence.AccountPersistenceAdapter;
+import com.ys.cleanarchiecture.adapter.persistence.ActivityRepository;
+import com.ys.cleanarchiecture.domain.AccountRepository;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
@@ -15,12 +23,33 @@ import org.springframework.stereotype.Component;
 @Component
 public @interface PersistenceAdapter {
 
-  /**
-   * The value may indicate a suggestion for a logical component name,
-   * to be turned into a Spring bean in case of an autodetected component.
-   * @return the suggested component name, if any (or empty String otherwise)
-   */
-  @AliasFor(annotation = Component.class)
-  String value() default "";
+	/**
+	 * The value may indicate a suggestion for a logical component name,
+	 * to be turned into a Spring bean in case of an autodetected component.
+	 *
+	 * @return the suggested component name, if any (or empty String otherwise)
+	 */
+	@AliasFor(annotation = Component.class)
+	String value() default "";
 
+}
+
+@Configuration
+@EnableJpaRepositories
+class PersistenceAdapterConfiguration {
+
+	@Bean
+	AccountPersistenceAdapter accountPersistenceAdapter(
+		AccountRepository accountRepository,
+		ActivityRepository activityRepository,
+		AccountMapper accountMapper) {
+
+		return new AccountPersistenceAdapter(
+		accountRepository, activityRepository, accountMapper);
+	}
+
+	@Bean
+	AccountMapper accountMapper() {
+		return new AccountMapper();
+	}
 }
