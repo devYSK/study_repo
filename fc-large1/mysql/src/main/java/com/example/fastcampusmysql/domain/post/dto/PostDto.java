2a4b5194d1eaa3e7d7638cfc919a714573aea740
ysk <kim206gh@naver.com>
2024-02-11 19:24:37 +0900

    add

--- fc-large1/mysql/src/main/java/com/example/fastcampusmysql/domain/post/dto/PostDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.fastcampusmysql.domain.post.dto;
-
-import java.time.LocalDateTime;
-
-public record PostDto(Long id, Long memberId, String contents, LocalDateTime createdAt, Long likeCount) {
-
-}
