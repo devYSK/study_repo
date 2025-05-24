7f50b5cd9083fef46967002fd56e9bf24aeb7d2a
ysk <kim206gh@naver.com>
2025-05-24 23:43:56 +0900

    add

---
+++ fc-chat/demo/part3/with-push/src/main/kotlin/com/yscorp/withpush/messagesystem/session/WebSocketSessionManager1.kt
@@ -0,0 +1,56 @@
+package com.yscorp.withpush.messagesystem.session
+
+import net.prostars.messagesystem.dto.domain.UserId
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import java.io.IOException
+import java.util.concurrent.ConcurrentHashMap
+
+@Component
+class WebSocketSessionManager(jsonUtil: JsonUtil) {
+    private val sessions: MutableMap<UserId, WebSocketSession> = ConcurrentHashMap<UserId, WebSocketSession>()
+    private val jsonUtil: JsonUtil = jsonUtil
+
+    fun getSessions(): List<WebSocketSession> {
+        return sessions.values.stream().toList()
+    }
+
+    fun getSession(userId: UserId): WebSocketSession? {
+        return sessions[userId]
+    }
+
+    fun putSession(userId: UserId, webSocketSession: WebSocketSession) {
+        log.info("Store Session : {}", webSocketSession.getId())
+        sessions[userId] = webSocketSession
+    }
+
+    fun closeSession(userId: UserId) {
+        try {
+            val webSocketSession: WebSocketSession? = sessions.remove(userId)
+            if (webSocketSession != null) {
+                log.info("Remove session : {}", userId)
+                webSocketSession.close()
+                log.info("Close session : {}", userId)
+            }
+        } catch (ex: Exception) {
+            log.error("WebSocketSession close failed. userId: {}", userId)
+        }
+    }
+
+    @Throws(IOException::class)
+    fun sendMessage(session: WebSocketSession, message: String) {
+        try {
+            session.sendMessage(TextMessage(message))
+            log.info("Send message: {} to {}", message, session.getId())
+        } catch (ex: Exception) {
+            log.error("Failed to send message to {} error: {}", session.getId(), ex.message)
+
+            throw ex
+        }
+    }
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(WebSocketSessionManager::class.java)
+    }
+}
